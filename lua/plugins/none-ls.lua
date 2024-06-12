27c033e0ce7cf62e530d3942fea6fb48d4b663ea
Pavel Mikhaylov <mikhailov.sztu@gmail.com>
2024-06-13 01:57:45 +0400

    improvements

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,5 +1,8 @@
 return {
 	"nvimtools/none-ls.nvim",
+  dependencies = {
+    'nvimtools/none-ls-extras.nvim'
+  },
 	config = function()
 		local null_ls = require("null-ls")
 
@@ -9,7 +12,7 @@ return {
 				null_ls.builtins.formatting.prettier,
 				null_ls.builtins.formatting.rubocop,
 				null_ls.builtins.diagnostics.rubocop,
-        require("none-ls.diagnostics.eslint"),
+        require("none-ls.diagnostics.eslint_d"),
 			},
 		})
 
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,7 +6,20 @@ return {
 		config = function()
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Find files" })
+			vim.keymap.set(
+				"n",
+				"<leader>ffh",
+				":Telescope find_files no_ignore=true<CR>",
+				{ desc = "Find files (with hidden)" }
+			)
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+			vim.keymap.set("n", "<leader>fgh", function()
+				builtin.live_grep({
+					additional_args = {
+						"-u",
+					},
+				})
+			end, { desc = "Live grep (with hidden)" })
 		end,
 	},
 	{
