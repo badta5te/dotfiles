211f7f446963c684a871587b9466d073b3be34d9
Pavel Mikhaylov <mikhailov.sztu@gmail.com>
2024-06-13 12:54:00 +0400

    improvements

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,8 +1,8 @@
 return {
 	"nvimtools/none-ls.nvim",
-  dependencies = {
-    'nvimtools/none-ls-extras.nvim'
-  },
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
 	config = function()
 		local null_ls = require("null-ls")
 
@@ -12,7 +12,7 @@ return {
 				null_ls.builtins.formatting.prettier,
 				null_ls.builtins.formatting.rubocop,
 				null_ls.builtins.diagnostics.rubocop,
-        require("none-ls.diagnostics.eslint_d"),
+				require("none-ls.diagnostics.eslint_d"),
 			},
 		})
 
