5716d35d348adfbc0af03ce8c901f549a8cd23dd
Pavel Mikhaylov <mikhailov.sztu@gmail.com>
2024-06-19 01:56:27 +0400

    add more plugins

--- init.lua
+++ init.lua
@@ -12,7 +12,7 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("vim-options")
-require("lazy").setup("plugins", {
+require("lazy").setup({ { import = "plugins" }, { import = "plugins.lsp" } }, {
 	checker = {
 		enabled = true,
 		notify = false,
