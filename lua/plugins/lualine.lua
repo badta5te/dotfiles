93973307092edd6c2bca2c870a90dec76c843043
Pavel Mikhaylov <mikhailov.sztu@gmail.com>
2024-06-22 19:18:18 +0400

    changes, fromatting

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,25 +1,30 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    local lazy_status = require("lazy.status")
+	"nvim-lualine/lualine.nvim",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		local lazy_status = require("lazy.status")
 
-    require("lualine").setup({
-      options = {
-        theme = "nord",
-      },
-      sections = {
-        lualine_x = {
-          {
-            lazy_status.updates,
-            cond = lazy_status.has_updates,
-            color = { fg = "#ff9e64" },
-          },
-          { "encoding" },
-          { "fileformat" },
-          { "filetype" },
-        },
-      },
-    })
-  end,
+		require("lualine").setup({
+			options = {
+				theme = "nord",
+			},
+			sections = {
+				lualine_x = {
+					{
+						require("noice").api.status.mode.get,
+						cond = require("noice").api.status.mode.has,
+						color = { fg = "#ff9e64" },
+					},
+					{
+						lazy_status.updates,
+						cond = lazy_status.has_updates,
+						color = { fg = "#ff9e64" },
+					},
+					{ "encoding" },
+					{ "fileformat" },
+					{ "filetype" },
+				},
+			},
+		})
+	end,
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,75 +1,88 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    branch = "0.1.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-      "nvim-tree/nvim-web-devicons",
-      "folke/todo-comments.nvim",
-    },
-    config = function()
-      local telescope = require("telescope")
-      local builtin = require("telescope.builtin")
-      local actions = require("telescope.actions")
-      local transform_mod = require("telescope.actions.mt").transform_mod
+	{
+		"nvim-telescope/telescope.nvim",
+		branch = "0.1.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+			"nvim-tree/nvim-web-devicons",
+			"folke/todo-comments.nvim",
+			"debugloop/telescope-undo.nvim",
+		},
+		config = function()
+			local telescope = require("telescope")
+			local builtin = require("telescope.builtin")
+			local actions = require("telescope.actions")
+			local transform_mod = require("telescope.actions.mt").transform_mod
 
-      local trouble = require("trouble")
-      local trouble_telescope = require("trouble.providers.telescope")
+			local trouble = require("trouble")
+			local trouble_telescope = require("trouble.providers.telescope")
 
-      -- or create your custom action
-      local custom_actions = transform_mod({
-        open_trouble_qflist = function(prompt_bufnr)
-          trouble.toggle("quickfix")
-        end,
-      })
+			-- or create your custom action
+			local custom_actions = transform_mod({
+				open_trouble_qflist = function(prompt_bufnr)
+					trouble.toggle("quickfix")
+				end,
+			})
 
-      local keymap = vim.keymap
+			local keymap = vim.keymap
 
-      telescope.load_extension("fzf")
+			telescope.setup({
+				defaults = {
+					path_display = { "smart" },
+					mappings = {
+						i = {
+							["<C-k>"] = actions.move_selection_previous, -- move to prev result
+							["<C-j>"] = actions.move_selection_next, -- move to next result
+							["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
+							["<C-t>"] = trouble_telescope.smart_open_with_trouble,
+						},
+					},
+				},
+				extensions = {
+					undo = {
+						-- telescope-undo.nvim config, see below
+					},
+					-- other extensions:
+					-- file_browser = { ... }
+				},
+			})
 
-      telescope.setup({
-        defaults = {
-          path_display = { "smart" },
-          mappings = {
-            i = {
-              ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-              ["<C-j>"] = actions.move_selection_next, -- move to next result
-              ["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
-              ["<C-t>"] = trouble_telescope.smart_open_with_trouble,
-            },
-          },
-        },
-      })
+			keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+			keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Fuzzy find recent files" })
+			keymap.set("n", "<leader>ffh", function()
+				builtin.find_files({ no_ignore = true, hidden = true })
+			end, { desc = "Find files (with hidden)" })
+			keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+			keymap.set("n", "<leader>fgh", function()
+				builtin.live_grep({
+					additional_args = {
+						"-u",
+					},
+				})
+			end, { desc = "Live grep (with hidden)" })
+			keymap.set("n", "<leader>fc", builtin.grep_string, { desc = "Find string under cursor in cwd" })
+			keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
+			keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffer" })
+			keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Show keymap" })
+			keymap.set("n", "<leader>u", "<cmd>Telescope undo<cr>")
 
-      keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
-      keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Fuzzy find recent files" })
-      keymap.set("n", "<leader>ffh", function()
-        builtin.find_files({ no_ignore = true, hidden = true })
-      end, { desc = "Find files (with hidden)" })
-      keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
-      keymap.set("n", "<leader>fgh", function()
-        builtin.live_grep({
-          additional_args = {
-            "-u",
-          },
-        })
-      end, { desc = "Live grep (with hidden)" })
-      keymap.set("n", "<leader>fc", builtin.grep_string, { desc = "Find string under cursor in cwd" })
-      keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
-    end,
-  },
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
-    config = function()
-      require("telescope").setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({}),
-          },
-        },
-      })
-      require("telescope").load_extension("ui-select")
-    end,
-  },
+			telescope.load_extension("fzf")
+			telescope.load_extension("undo")
+			telescope.load_extension("noice")
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
