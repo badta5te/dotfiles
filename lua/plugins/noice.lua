e2ebcd6d12abe2a3ab065aa1d87446246dbd1f96
Pavel Mikhaylov <mikhailov.sztu@gmail.com>
2024-06-22 19:17:17 +0400

    add noice

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,23 @@
+return {
+	"folke/noice.nvim",
+	event = "VeryLazy",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "rcarriga/nvim-notify",
+  },
+	opts = {
+		routes = {
+			{
+				filter = { event = "notify", find = "No information available" },
+				opts = { skip = true },
+			},
+			{
+				filter = { event = "msg_show", kind = "", find = "written" },
+				opts = { skip = true },
+			},
+		},
+		presets = {
+			lsp_doc_border = true,
+		},
+	},
+}
